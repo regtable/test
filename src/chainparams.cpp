f9e74112af26eb56f7c2ad936b0a1cf06e5e0d49
regtable <ghettomining@gmail.com>
2022-11-13 21:27:08 +0000

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -36,6 +36,7 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
     genesis.vtx.push_back(MakeTransactionRef(std::move(txNew)));
     genesis.hashPrevBlock.SetNull();
     genesis.hashMerkleRoot = BlockMerkleRoot(genesis);
+    LogPrintf("%s",genesis.ToString())
     return genesis;
 }
 
