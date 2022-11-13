fad1d60eb2db608b744675e470c86a62bbcca7aa
regtable <ghettomining@gmail.com>
2022-11-13 18:32:53 +0000

    Update kernel.cpp

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -50,13 +50,13 @@ const unsigned int nProtocolV10TestSwitchTime = 1625140800; // Thu  1 Jul 12:00:
 // Hard checkpoints of stake modifiers to ensure they are deterministic
 static std::map<int, unsigned int> mapStakeModifierCheckpoints =
     boost::assign::map_list_of
-    ( 0, 0x )
+    ( 0, 0x0e00670bu )
 
     ;
 
 static std::map<int, unsigned int> mapStakeModifierTestnetCheckpoints =
     boost::assign::map_list_of
-    ( 0, 0x )
+    ( 0, 0x0e00670bu )
 
     ;
 
