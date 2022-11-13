fc348b2f56aa5646df127d3d3dee15fa2f7c5b8e
regtable <ghettomining@gmail.com>
2022-11-13 18:58:15 +0000

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -55,7 +55,7 @@ static CBlock CreateGenesisBlock(uint32_t nTimeTx, uint32_t nTimeBlock, uint32_t
 {
     const char* pszTimestamp = "zippy is a drug addict";
     const CScript genesisOutputScript = CScript();
-LogPrintf(CScript().ToString());
+LogPrintf(CScript());
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTimeTx, nTimeBlock, nNonce, nBits, nVersion, genesisReward);
 }
 
